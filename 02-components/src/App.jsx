4a997a65e558ad17c238dcf08ee97bf7943feff1
Eliza khan <elizakhan288@gmail.com>
2025-10-16 15:52:33 +0530

    02-components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='card'>
+        <h1>Eliza</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className='card'>
+        <h1>Eliza</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
